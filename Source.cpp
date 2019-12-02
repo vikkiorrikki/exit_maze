1f008482d54f1b3fd661fa34d68b2b512d637ab0
Saklakova <victoria.saklakova@arcadia.spb.ru>
2019-12-02 20:01:31 +0300

    changes for algorithm

--- Source.cpp
+++ Source.cpp
@@ -1,16 +1,18 @@
-#include<iostream>
+п»ї#include<iostream>
 #include<fstream>
 #include<string>
 #include<vector>
+#include<queue>
+#include<list>
 
 using namespace std;
 
-//как хранить лабиринт. точки, координаты
-//алгоритм работы с лабиринтом
+//РєР°Рє С…СЂР°РЅРёС‚СЊ Р»Р°Р±РёСЂРёРЅС‚. С‚РѕС‡РєРё, РєРѕРѕСЂРґРёРЅР°С‚С‹
+//Р°Р»РіРѕСЂРёС‚Рј СЂР°Р±РѕС‚С‹ СЃ Р»Р°Р±РёСЂРёРЅС‚РѕРј
 
 class Point {
 public:
-	Point(){
+	Point() {
 		x = y = 0;
 		block = false;
 	}
@@ -46,57 +48,67 @@ public:
 private:
 	int x;
 	int y;
-	//bool border; //граница лабиринта
-	bool block; //препятствие лабиринта
+	//bool border; //РіСЂР°РЅРёС†Р° Р»Р°Р±РёСЂРёРЅС‚Р°
+	bool block; //РїСЂРµРїСЏС‚СЃС‚РІРёРµ Р»Р°Р±РёСЂРёРЅС‚Р°
 	bool start = false;
 	bool finish = false;
 };
 
+void AStar(Point start, Point finish) {
+	priority_queue<Point> open_points;
+	list<Point> close_points;
+
+
+	//return РїСѓС‚СЊ start to finish
+}
 
 int main() {
-	string path = "d:\\projects\\git\\exit_maze\\ConsoleApplication1\\maze.txt";
+	//string path = "d:\\projects\\git\\exit_maze\\ConsoleApplication1\\maze.txt";
+	string path = "c:\\study\\github\\exit_maze_project\\Project1\\maze.txt";
 	ifstream fmaze;
 	fmaze.open(path);
 
 	vector<Point> maze;
 
-	 if (!fmaze.is_open()) {
-		 cout << "File is not opened!" << endl;
-	 }
-	 else {
-		 cout << "File is opened!" << endl;
-		 
-		 char ch;
-		 Point pnt;
+	if (!fmaze.is_open()) {
+		cout << "File is not opened!" << endl;
+	}
+	else {
+		cout << "File is opened!" << endl;
 
-		 for (int y = 0; fmaze.get(ch); y++)
-		 {
-			 for (int x = 0; ch != '\n'; x++)
-			 {
-				 if (ch == '*') {
-					 pnt.setX(x);
-					 pnt.setY(y);
-					 pnt.setBlock(true);
-					 pnt.setStart(false);
-					 pnt.setFinish(false);
-					 maze.push_back(pnt);
-					 fmaze.get(ch);
-				 }
-				 else {
-					 pnt.setX(x);
-					 pnt.setY(y);
-					 pnt.setBlock(false);
-					 if (pnt.getY() == 0) pnt.setStart(true);
-					 if (pnt.getY() == 5) pnt.setFinish(true);
-					 maze.push_back(pnt);
-					 fmaze.get(ch);
-				 }
-			 } 
-		 }
-	 }
-	 for (int i = 0; i < maze.size(); i++) {
-		 maze[i].Print();
-	 }
+		char ch;
+		Point pnt;
+
+		for (int y = 0; fmaze.get(ch); y++)
+		{
+			for (int x = 0; ch != '\n'; x++)
+			{
+				if (ch == '*') {
+					pnt.setX(x);
+					pnt.setY(y);
+					pnt.setBlock(true);
+					pnt.setStart(false);
+					pnt.setFinish(false);
+					maze.push_back(pnt);
+					fmaze.get(ch);
+				}
+				else {
+					pnt.setX(x);
+					pnt.setY(y);
+					pnt.setBlock(false);
+					if (pnt.getY() == 0) pnt.setStart(true);
+					if (pnt.getY() == 5) pnt.setFinish(true);
+					maze.push_back(pnt);
+					fmaze.get(ch);
+				}
+			}
+		}
+	}
+	for (int i = 0; i < maze.size(); i++) {
+		maze[i].Print();
+	}
+
+
 
 	fmaze.close();
 	system("pause");
